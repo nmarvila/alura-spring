bdcd313cf7994dead881af963cf21b42cb77b27c
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-10-15 12:21:23 -0300

    Committing "Java Servlet" fifth lesson

--- workspace/gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ workspace/gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,6 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -21,8 +24,19 @@ public class NovaEmpresaServlet extends HttpServlet {
 		System.out.println("Cadastrando nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		String paramDataEmpresa = request.getParameter("data");
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
