a0367ad49a1e14b1c07dc69c1db974dc7c4bb93f
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-10-18 12:07:56 -0300

    Committing "Java Servlet - Parte 2" third lesson

---
+++ workspace/gerenciador/src/br/com/alura/gerenciador/acao/Login.java
@@ -0,0 +1,34 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Usuario;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Login implements Acao {
+
+	@Override
+	public String executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		System.out.println("Logando " + login);
+		
+		Banco banco = new Banco();
+		Usuario usuario = banco.existeUsuario(login, senha);
+		
+		if(usuario != null) {
+			System.out.println("Usuario existe");
+			return "redirect:entrada?acao=ListaEmpresas";
+		} else {
+			return "redirect:entrada?acao=LoginForm";
+		}
+		
+	}
+
+}
