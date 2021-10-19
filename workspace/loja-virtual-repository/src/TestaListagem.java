8717f5f5d14e49046447ad373b28e0bea3e18ff3
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-10-19 17:43:20 -0300

    Committing "Java e JDBC" second lesson

---
+++ workspace/loja-virtual-repository/src/TestaListagem.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+		
+		Statement stm = connection.createStatement();
+		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		
+		ResultSet rst = stm.getResultSet();
+		
+		while(rst.next()) {
+			Integer id = rst.getInt("ID");
+			String nome = rst.getString("NOME");
+			String descricao = rst.getString("DESCRICAO");
+			System.out.println(id);
+			System.out.println(nome);
+			System.out.println(descricao);
+		}
+		
+		connection.close();
+		
+	}
+
+}
