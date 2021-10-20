14cfbe4ed01c180ab108f66d6e27f5d3862330ae
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-10-20 18:03:31 -0300

    Committing "JPA" fourth lesson

--- workspace/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ workspace/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,32 +1,30 @@
 package br.com.alura.loja.testes;
 
-import java.math.BigDecimal;
-
 import javax.persistence.EntityManager;
 
-import br.com.alura.loja.dao.CategoriaDao;
-import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
-import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 		Categoria celulares = new Categoria("CELULARES");
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
 		
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDao produtoDao = new ProdutoDao(em);
-		CategoriaDao categoriaDao = new CategoriaDao(em);
-		
 		em.getTransaction().begin();
 		
-		categoriaDao.cadastrar(celulares);
-		produtoDao.cadastrar(celular);
+		em.persist(celulares);
+		celulares.setNome("XPTO");
 		
-		em.getTransaction().commit();
-		em.close();
+		em.flush();
+		em.clear();
+		
+		celulares = em.merge(celulares);
+		celulares.setNome("1234");
+		em.flush();
+		em.clear();
+		em.remove(celulares);
+		em.flush();
 	}
 
 }
