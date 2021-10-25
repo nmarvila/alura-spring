5bf75bc14bf3e1a30b3b51d3349e2be6eb14b2ef
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-10-25 17:39:34 -0300

    Committing "Spring MVC - Parte 2" third lesson

--- workspace/mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ workspace/mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -21,15 +21,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-			.anyRequest().authenticated()
+		.antMatchers("/home/**")
+			.permitAll()
+		.anyRequest()
+			.authenticated()
 		.and()
 		.formLogin(form -> form
             .loginPage("/login")
-            .defaultSuccessUrl("/home", true)
+            .defaultSuccessUrl("/usuario/pedido", true)
             .permitAll()
         )
-		.logout(logout -> logout.logoutUrl("/logout"))
-		.csrf().disable();
+		.logout(logout -> {
+			logout.logoutUrl("/logout")
+				.logoutSuccessUrl("/home");
+		});
 	}
 	
 	@Override
