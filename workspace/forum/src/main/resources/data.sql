f5e9e5bd1e047d504810a846d76322688d252745
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-10-28 17:53:27 -0300

    Committing "Spring Boot API REST - Parte 2" third lesson

--- workspace/forum/src/main/resources/data.sql
+++ workspace/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$qMJb6NtWuyNiLbS5yRh.nuDIqyBgQFn2C0EsEDg77xJe0EkYev0/S');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
